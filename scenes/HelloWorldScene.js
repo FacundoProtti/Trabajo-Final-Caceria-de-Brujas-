48d505697a8a7b82e999353ef6d1b4c42536b2ad
FacundoProtti <167729764+FacundoProtti@users.noreply.github.com>
2025-05-05 21:37:35 -0300

    initial commit

--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -1,46 +1,168 @@
-// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
-
-export default class HelloWorldScene extends Phaser.Scene {
+export default class Game extends Phaser.Scene {
   constructor() {
-    // key of the scene
-    // the key will be used to start the scene by other scenes
-    super("hello-world");
+    super("game");
   }
 
   init() {
-    // this is called before the scene is created
-    // init variables
-    // take data passed from other scenes
-    // data object param {}
+    this.initialTime = 30;
+    this.collectedShapes = [];
   }
 
   preload() {
     // load assets
-    this.load.image("sky", "./assets/space3.png");
-    this.load.image("logo", "./assets/phaser3-logo.png");
-    this.load.image("red", "./assets/particles/red.png");
+    this.load.image("Cielo", "./public/assets/Cielo.webp");
+    this.load.image("Diamante", "./public/assets/diamond.png");
+    this.load.image("Menu", "./public/assets/FondoMenu.jpg");
+    this.load.image("Ninja", "./public/assets/Ninja.png");
+    this.load.image("Plataforma", "./public/assets/platform.png");
+    this.load.image("Cuadrado", "./public/assets/square.png");
+    this.load.image("Triangulo", "./public/assets/triangle.png");
+  }
+
+  createRandomShape() {
+    const tipos = ["Diamante", "Triangulo", "Cuadrado"];
+    const tipoElegido = Phaser.Utils.Array.GetRandom(tipos);
+    const objeto = this.shapes.create(Phaser.Math.Between(50, 750), 0, tipoElegido);
+    objeto.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    objeto.setCollideWorldBounds(true);
   }
 
   create() {
-    // create game objects
-    this.add.image(400, 300, "sky");
+    this.add.image(400, 300, "Cielo").setScale(2);
 
-    const logo = this.physics.add.image(400, 100, "logo");
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    this.shapes = this.physics.add.group();
 
-    // emmit particles from logo
-    const emitter = this.add.particles(0, 0, "red", {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(400, 568, "Plataforma").setScale(2).refreshBody();
+    this.platforms.create(400, 200, "Plataforma");
+    this.platforms.create(50, 400, "Plataforma");
+    this.platforms.create(750, 400, "Plataforma");
+
+    this.player = this.physics.add.sprite(100, 450, "Ninja").setScale(0.1);
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+    
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.rKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+
+    this.score = 0;
+    this.gameOver = false;
+
+    this.scoreText = this.add.text(16, 16, `Score: ${this.score}`, {
+      fontSize: "32px",
+      fill: "#000",
     });
 
-    emitter.startFollow(logo);
+    this.timerText = this.add.text(600, 16, `Time: ${this.initialTime}`, {
+      fontSize: "32px",
+      fill: "#000",
+    });
+
+    this.timeEvent = this.time.addEvent({
+      delay: 1000,
+      callback: this.onSecond,
+      callbackScope: this,
+      loop: true,
+    });
+
+    this.gameOverText = this.add.text(400, 300, "GAME OVER", {
+      fontSize: "64px",
+      fill: "#ff0000",
+    });
+    this.gameOverText.setOrigin(0.5);
+    this.gameOverText.setVisible(false);
+
+    this.shapeTimer = this.time.addEvent({
+      delay: 1000,
+      callback: this.createRandomShape,
+      callbackScope: this,
+      loop: true,
+    });
+
+    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.shapes, this.platforms);
+this.physics.add.overlap(this.player, this.shapes, this.collectShape, null, this);
   }
 
   update() {
-    // update game objects
+    if (this.gameOver) {
+      if (Phaser.Input.Keyboard.JustDown(this.rKey)) {
+        this.scene.restart();
+      }
+      return;
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+    } else {
+      this.player.setVelocityX(0);
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
+  }
+
+  collectShape(player, shape) {
+    shape.disableBody(true, true);
+
+    let valor = 0;
+    switch (shape.texture.key) {
+        case "Diamante":
+            valor = 20;
+            break;
+        case "Triangulo":
+            valor = 10;
+            break;
+        case "Cuadrado":
+            valor = 5;
+            break;
+    }
+
+    this.score += valor;
+    this.scoreText.setText(`Score: ${this.score}`);
+    this.collectedShapes.push(shape.texture.key);
+
+    const counts = {
+        Diamante: 0,
+        Triangulo: 0,
+        Cuadrado: 0,
+    };
+
+    this.collectedShapes.forEach(item => {
+        if (counts[item] !== undefined) {
+            counts[item]++;
+        }
+    });
+
+    if (counts.Diamante >= 2 && counts.Triangulo >= 2 && counts.Cuadrado >= 2) {
+        this.physics.pause();
+        this.player.setTint(0x00ff00);
+        this.add.text(400, 300, "GANASTE!", {
+            fontSize: "64px",
+            fill: "#00ff00",
+        }).setOrigin(0.5);
+        this.shapeTimer.remove();
+        this.timeEvent.remove();
+        this.gameOver = true;
+      }
+}
+onSecond() {
+  if (!this.gameOver) {
+    this.initialTime -= 1;
+    this.timerText.setText('Time: ' + this.initialTime);
+
+    if (this.initialTime <= 0) {
+      this.physics.pause();
+      this.player.setTint(0xff0000);
+      this.gameOver = true;
+      this.gameOverText.setVisible(true);
+    }
   }
 }
+}
+
+
